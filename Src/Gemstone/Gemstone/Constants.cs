a41e053c66fce6c453721e24195c0eea8ca5e509
Lexi <reeseivey21@hotmail.com>
2026-06-03 17:50:45 -0400

    Update Constants.cs

--- Src/Gemstone/Gemstone/Constants.cs
+++ Src/Gemstone/Gemstone/Constants.cs
@@ -1,10 +1,10 @@
-﻿namespace Gemstone.Gemstone
+namespace Gemstone.Gemstone
 {
     internal class Constants
     {
         public const string GUID = "Lexi.Gemstone.Menu";
         public const string Name = "Gemstone.Menu";
-        public const string Version = "1.1.5";
-        public const bool Debug = false;
+        public const string Version = "1.1.6";
+        public const bool Debug = true;
     }
 }
